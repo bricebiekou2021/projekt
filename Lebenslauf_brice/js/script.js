67546347c86ab002eb7c1f270051854f988d644d
unknown <bricebiekou@yahoo.fr>
2025-01-07 16:01:07 +0100

    first commit

---
+++ Lebenslauf_brice/js/script.js
@@ -0,0 +1,9 @@
+
+// Typed js
+const typed = new Typed('.multiple-text', {
+    strings: ['Informatiker' , 'Frontend Entwickler', 'Backend Entwickler' , 'Fluustack Software Web Entwickler' , 'MBA'],
+    typeSpeed: 100,
+    backSpeed : 100,
+    backDelay: 1000,
+    loop: true
+});
